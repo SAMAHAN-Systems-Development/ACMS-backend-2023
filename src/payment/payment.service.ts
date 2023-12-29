2846f72ac8157a81cc8be85bf23e8fa049ab9f61
Ivan <ivansamwabina@gmail.com>
2023-12-29 20:08:03 +0800

    feat: read declined payments with pagination

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -6,9 +6,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class PaymentService {
   constructor(private prisma: PrismaService) {}
 
-  async findAllAccepted(page = 1, items = 10): Promise<Payment[]> {
+  async findAllAcceptedPayments(page = 1, items = 10): Promise<Payment[]> {
     return this.prisma.payment.findMany({
       where: { status: 'accepted' },
+      take: items,
+      skip: items * (page - 1),
+    });
+  }
+  async findAllDeclinedPayments(page = 1, items = 10): Promise<Payment[]> {
+    return this.prisma.payment.findMany({
+      where: { status: 'declined' },
       take: items,
       skip: items * (page - 1),
     });

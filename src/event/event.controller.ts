5f895773d3e5d0422884244c3bcbf12832830a4d
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2023-12-30 02:14:28 +0800

    feat: added ability to activate and inactivate an event

---
+++ src/event/event.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { EventService } from './event.service';
+
+@Controller('event')
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+
+  @Patch('/activate/:id')
+  async activateEvent(@Param('id') eventId: number) {
+    try {
+      return await this.eventService.activateEvent(eventId);
+    } catch (error) {
+      throw new HttpException(
+        'Unable to activate event.',
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
+  }
+
+  @Patch('/inactivate/:id')
+  async inactivateEvent(@Param('id') eventId: number) {
+    try {
+      return this.eventService.inactivateEvent(eventId);
+    } catch (error) {
+      throw new HttpException(
+        'Unable to inactivate event.',
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
+  }
+}

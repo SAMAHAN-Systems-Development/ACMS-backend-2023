c293c3605ab010db53d0078a0d206eab9ee00f43
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2024-01-01 20:16:14 +0800

    chore: removed stray TODO comments

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -8,7 +8,7 @@ export class EventService {
   async activateEvent(eventId: number) {
     const event = this.prismaService.event.update({
       data: { is_active: true },
-      where: { id: eventId }, // TODO: Use DTO to implicit type cast
+      where: { id: eventId },
       select: {
         title: true,
         is_active: true,
@@ -20,7 +20,7 @@ export class EventService {
   async inactivateEvent(eventId: number) {
     const event = this.prismaService.event.update({
       data: { is_active: false },
-      where: { id: eventId }, // TODO: Use DTO to implicit type cast
+      where: { id: eventId },
       select: {
         title: true,
         is_active: true,

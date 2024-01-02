2ed8afefb592735fedcdcadbedfb8e3d0ab88a8b
Ivan <ivansamwabina@gmail.com>
2024-01-02 21:07:06 +0800

    feat: read all accepted payments
    Retrieve all accepted payments, including associated events and students, with pagination set at 10 data entries per page.

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Payment } from '@prisma/client';
+import { Student } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PaymentService {
   constructor(private prisma: PrismaService) {}
 
-  async findAllAcceptedPayments(page = 1, items = 10): Promise<Payment[]> {
-    return this.prisma.payment.findMany({
-      where: { status: 'accepted' },
-      take: items,
-      skip: items * (page - 1),
-    });
-  }
-
-  async findAllDeclinedPayments(page = 1, items = 10): Promise<Payment[]> {
-    return this.prisma.payment.findMany({
-      where: { status: 'declined' },
+  async findAllAcceptedPayments(page = 1, items = 10): Promise<Student[]> {
+    return this.prisma.student.findMany({
+      include: {
+        payment: {},
+        event: {},
+      },
+      where: {
+        payment: {
+          status: 'accepted',
+        }
+      },
       take: items,
       skip: items * (page - 1),
     });

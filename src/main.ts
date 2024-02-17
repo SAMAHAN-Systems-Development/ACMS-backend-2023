2da27a9bf6e71ca332fd81b890f62551b3025f4a
Viver Bungag <viverbungag1@gmail.com>
2024-02-17 20:42:13 +0800

    chore: configured temporarily to enable cors for all url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: '*',
   });
   await app.listen(3000);
 }
--- supabase/supabase.service.ts
+++ supabase/supabase.service.ts
@@ -9,8 +9,6 @@ export class SupabaseService {
   constructor() {
     const supabaseURL = process.env.SUPABASE_URL;
     const supabaseAPIKey = process.env.SUPABASE_KEY;
-
-    console.log('Supabase URL:', supabaseURL); // Log the URL before creating the client
     this.supabase = createClient(supabaseURL, supabaseAPIKey);
   }
 

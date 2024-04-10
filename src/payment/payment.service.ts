317b6fe49faa04d89afc50de349eea3ba416a6ed
Viver Bungag <viverbungag1@gmail.com>
2024-04-11 00:36:08 +0800

    fix: added fake data

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -261,33 +261,14 @@ export class PaymentService {
     });
 
     const toReturnPendingPayments = pendingPayments.map((pendingPayment) => {
-      let event = pendingPayment?.student?.eventTierOnEvent?.event;
-      let eventTier = pendingPayment?.student?.eventTierOnEvent?.eventTier;
+      const event = pendingPayment?.student?.eventTierOnEvent?.event;
+      const eventTier = pendingPayment?.student?.eventTierOnEvent?.eventTier;
 
       if (!event || !eventTier) {
-        console.log('---- Inside no event or eventTier ----');
         console.log(pendingPayment);
-        eventTier = {
-          id: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          name: 'Sample',
-          is_active: true,
-        };
-        event = {
-          id: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          title: 'Sample',
-          description: 'Sample',
-          date: new Date(),
-          is_active: true,
-          form_name: 'Sample',
-          requires_payment: true,
-          earlyBirdAccessDate: new Date(),
-          hasEarlyBirdAccess: true,
-        };
+        return this.sampleData;
       }
+
       const finalPendingPayment = {
         ...pendingPayment,
         event: event,
@@ -387,4 +368,82 @@ export class PaymentService {
     });
     return pendingPayments;
   }
+
+  sampleData = {
+    id: 100,
+    createdAt: '2024-04-05T09:44:19.984Z',
+    updatedAt: '2024-04-10T14:23:41.277Z',
+    photo_src:
+      'https://fjqloxpyknqccretzoyt.supabase.co/storage/v1/object/public/payment/yxbcua_2BS%20Chemical%20Engineering_E1',
+    status: 'pending',
+    required_payment: 500,
+    student: {
+      id: 87,
+      createdAt: '2024-04-05T09:44:20.119Z',
+      updatedAt: '2024-04-05T09:44:20.119Z',
+      uuid: '14c4ec3d-c884-4b96-94fa-658c0f379bb2',
+      firstName: 'FAKE DATA',
+      lastName: 'FAKE DATA',
+      email: 'FAKE_DATA@gmail.com',
+      year_and_course: 'FAKE DATA',
+      requires_payment: true,
+      paymentId: 100,
+      eventTierOnEventId: 4,
+      is_addu_student: true,
+      control_number: 'MADAYAW0103020',
+      eventTierOnEvent: {
+        id: 4,
+        createdAt: '2024-03-30T08:25:02.334Z',
+        updatedAt: '2024-04-01T16:31:42.147Z',
+        eventId: 1,
+        eventTierId: 4,
+        earlyBirdPrice: 0,
+        originalPrice: 500,
+        max_participants: 50,
+        is_active: true,
+        event: {
+          id: 1,
+          createdAt: '2024-03-30T08:25:02.247Z',
+          updatedAt: '2024-04-07T09:01:05.199Z',
+          title: 'Madayaw Nights 2024 Early Bird',
+          description:
+            'The Early Bird edition for the Madayaw Nights 2024 Event',
+          date: '2024-04-20T00:00:00.000Z',
+          is_active: true,
+          form_name: 'madayaw-nights-2024-early-bird',
+          requires_payment: true,
+          earlyBirdAccessDate: '2024-03-31T00:00:00.000Z',
+          hasEarlyBirdAccess: false,
+        },
+        eventTier: {
+          id: 4,
+          createdAt: '2024-03-30T08:04:08.094Z',
+          updatedAt: '2024-03-30T08:04:08.094Z',
+          name: 'Bronze',
+          is_active: true,
+        },
+      },
+    },
+    event: {
+      id: 1,
+      createdAt: '2024-03-30T08:25:02.247Z',
+      updatedAt: '2024-04-07T09:01:05.199Z',
+      title: 'Madayaw Nights 2024 Early Bird',
+      description: 'The Early Bird edition for the Madayaw Nights 2024 Event',
+      date: '2024-04-20T00:00:00.000Z',
+      is_active: true,
+      form_name: 'madayaw-nights-2024-early-bird',
+      requires_payment: true,
+      earlyBirdAccessDate: '2024-03-31T00:00:00.000Z',
+      hasEarlyBirdAccess: false,
+    },
+    eventTier: {
+      id: 4,
+      createdAt: '2024-03-30T08:04:08.094Z',
+      updatedAt: '2024-03-30T08:04:08.094Z',
+      name: 'Bronze',
+      is_active: true,
+    },
+    eventPrice: 500,
+  };
 }

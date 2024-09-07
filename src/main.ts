08b237f47dada92d091171c209b0fc5fe8a5b1fc
Rhaey <rhaeyislyfe@gmail.com>
2024-09-07 11:38:02 +0800

    feat: add default frontend_url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
   });
 
   const config = new DocumentBuilder()
